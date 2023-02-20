00e75c86666b5e4e8f4d2c1301ebc4c8d1f604b6
msiglreith <m.siglreith@gmail.com>
2023-02-20 21:37:01 +0100

    vello: compact code

--- nari-vello/examples/canvas.rs
+++ nari-vello/examples/canvas.rs
@@ -1,4 +1,5 @@
 use nari_platform::{ControlFlow, Event, Platform, SurfaceArea};
+use nari_vello::Canvas;
 use std::collections::HashMap;
 use vello::{
     kurbo::{Affine, Point, Rect},
@@ -6,87 +7,19 @@ use vello::{
     Scene, SceneBuilder, SceneFragment,
 };
 
-fn render_text_run(
-    sb: &mut SceneBuilder,
-    text_run: &nari_vello::typo::TextRun,
-    affine: Affine,
-    align_x: nari_vello::Align,
-    brush: Brush,
-    glyph_cache: &nari_vello::typo::GlyphCache,
-) {
-    let px = text_run.offset_x(align_x);
-    for cluster in &text_run.clusters {
-        for glyph in &cluster.glyphs {
-            let key = nari_vello::typo::GlyphKey {
-                id: glyph.id,
-                offset: glyph.offset.fract(),
-            };
-            let path = glyph_cache
-                .get(&(text_run.font.size, key))
-                .expect("missing glyph entry");
-            let advance = px + glyph.offset.trunc().f64();
-
-            sb.fill(
-                Fill::NonZero,
-                Affine::translate((advance as _, 0.0))
-                    * affine
-                    * Affine::scale_non_uniform(1.0, -1.0),
-                &brush,
-                None,
-                &path,
-            );
-        }
-    }
-}
-
 async fn run() -> anyhow::Result<()> {
     let platform = Platform::new();
 
-    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
-        backends: wgpu::Backends::VULKAN,
-        dx12_shader_compiler: wgpu::Dx12Compiler::Fxc,
-    });
-    let adapter = instance.request_adapter(&Default::default()).await.unwrap();
-    let features = adapter.features();
-    let limits = wgpu::Limits::default();
-    let (device, queue) = adapter
-        .request_device(
-            &wgpu::DeviceDescriptor {
-                label: None,
-                features: features
-                    & (wgpu::Features::TIMESTAMP_QUERY | wgpu::Features::CLEAR_TEXTURE),
-                limits,
-            },
-            None,
-        )
-        .await?;
+    let mut canvas = Canvas::new(platform.surface).await;
 
-    let mut renderer = vello::Renderer::new(&device).unwrap();
-    let mut engine = nari_vello::Engine::new();
-    let mut glyph_cache = nari_vello::typo::GlyphCache::default();
-
-    let font = engine.create_font(std::fs::read("assets/segoeui.ttf")?);
+    let font = canvas.create_font(std::fs::read("assets/segoeui.ttf")?);
     let mut font_table = HashMap::<nari_vello::typo::FontSize, _>::default();
 
     for ft in 6..25 {
-        font_table.insert(ft, engine.create_font_scaled(font, ft));
+        font_table.insert(ft, canvas.create_font_scaled(font, ft));
     }
 
-    let surface = unsafe { instance.create_surface(&platform.surface)? };
-    let mut size = platform.surface.extent();
-    let mut surface_config = wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format: wgpu::TextureFormat::Bgra8UnormSrgb,
-        width: size.width,
-        height: size.height,
-        present_mode: wgpu::PresentMode::AutoNoVsync,
-        alpha_mode: wgpu::CompositeAlphaMode::Opaque,
-        view_formats: vec![],
-    };
-    surface.configure(&device, &surface_config);
-
     let mut scene = Scene::default();
-
     let mut waterfall = SceneFragment::default();
     {
         let mut sb = SceneBuilder::for_fragment(&mut waterfall);
@@ -94,32 +27,26 @@ async fn run() -> anyhow::Result<()> {
 
         for ft in 6..25 {
             let font = &font_table[&ft];
-            let text_run = engine.build_text_run(
-                *font,
-                &format!("{}: The lazy dog 0123456789", ft),
-                &mut glyph_cache,
-            );
-            render_text_run(
+            let text_run =
+                canvas.build_text_run(*font, &format!("{}: The lazy dog 0123456789", ft));
+            canvas.text_run(
                 &mut sb,
                 &text_run,
                 Affine::translate((0.0, py as _)),
                 nari_vello::Align::Positive,
                 vello::peniko::Brush::Solid(Color::rgb(1.0, 1.0, 1.0)),
-                &glyph_cache,
             );
             py += font.properties.height;
         }
         sb.finish();
     }
 
+    let mut size = platform.surface.extent();
     platform.run(move |event_loop, event| {
         match event {
             Event::Resize(extent) => {
                 size = extent;
-                surface_config.width = extent.width;
-                surface_config.height = extent.height;
-                surface.configure(&device, &surface_config);
-
+                canvas.resize(extent);
                 event_loop.surface.redraw();
             }
 
@@ -169,27 +96,10 @@ async fn run() -> anyhow::Result<()> {
                 sb.append(&waterfall, Some(Affine::translate((20.0, 20.0))));
 
                 sb.finish();
-                // std::thread::sleep(std::time::Duration::from_millis(10));
                 superluminal_perf::end_event();
                 println!("{:?}", t0.elapsed());
 
-                let frame_image = surface
-                    .get_current_texture()
-                    .expect("failed to get surface texture");
-
-                renderer
-                    .render_to_surface(
-                        &device,
-                        &queue,
-                        &scene,
-                        &frame_image,
-                        size.width,
-                        size.height,
-                    )
-                    .expect("failed to render to surface");
-
-                frame_image.present();
-                device.poll(wgpu::Maintain::Poll);
+                canvas.present(&scene);
             }
             _ => (),
         }
--- nari-vello/src/lib.rs
+++ nari-vello/src/lib.rs
@@ -3,10 +3,153 @@ mod fxp;
 pub mod typo;
 
 pub use self::engine::Engine;
+use self::typo::{Font, FontScaled, FontSize, GlyphCache, GlyphKey, TextRun};
+
+use nari_platform::{Extent, Surface};
+use std::pin::Pin;
+use vello::{
+    kurbo::Affine,
+    peniko::{Brush, Fill},
+    Scene, SceneBuilder,
+};
 
 #[derive(Debug, Clone, Copy)]
 pub enum Align {
     Negative,
     Center,
     Positive,
+}
+
+pub struct Canvas {
+    _instance: wgpu::Instance,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    swapchain: wgpu::Surface,
+    swapchain_config: wgpu::SurfaceConfiguration,
+    engine: Pin<Box<Engine>>,
+    renderer: vello::Renderer,
+    glyph_cache: typo::GlyphCache,
+}
+
+impl Canvas {
+    pub async fn new(surface: Surface) -> Self {
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor::default());
+        let adapter = instance.request_adapter(&Default::default()).await.unwrap();
+        let features = adapter.features();
+        let limits = wgpu::Limits::default();
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    features: features
+                        & (wgpu::Features::TIMESTAMP_QUERY | wgpu::Features::CLEAR_TEXTURE),
+                    limits,
+                },
+                None,
+            )
+            .await
+            .unwrap();
+
+        let swapchain = unsafe { instance.create_surface(&surface).unwrap() };
+        let size = surface.extent();
+        let swapchain_config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::AutoNoVsync,
+            alpha_mode: wgpu::CompositeAlphaMode::Opaque,
+            view_formats: vec![],
+        };
+        swapchain.configure(&device, &swapchain_config);
+
+        let renderer = vello::Renderer::new(&device).unwrap();
+        let engine = Engine::new();
+        let glyph_cache = GlyphCache::default();
+
+        Self {
+            _instance: instance,
+            device,
+            swapchain,
+            swapchain_config,
+            queue,
+            engine,
+            renderer,
+            glyph_cache,
+        }
+    }
+
+    pub fn present(&mut self, scene: &Scene) {
+        let frame_image = self
+            .swapchain
+            .get_current_texture()
+            .expect("failed to get surface texture");
+
+        self.renderer
+            .render_to_surface(
+                &self.device,
+                &self.queue,
+                scene,
+                &frame_image,
+                self.swapchain_config.width,
+                self.swapchain_config.height,
+            )
+            .expect("failed to render to surface");
+
+        frame_image.present();
+        self.device.poll(wgpu::Maintain::Poll);
+    }
+
+    pub fn resize(&mut self, size: Extent) {
+        self.swapchain_config.width = size.width;
+        self.swapchain_config.height = size.height;
+        self.swapchain
+            .configure(&self.device, &self.swapchain_config);
+    }
+
+    pub fn create_font(&mut self, data: Vec<u8>) -> Font {
+        self.engine.create_font(data)
+    }
+
+    pub fn create_font_scaled(&mut self, font: Font, size: FontSize) -> FontScaled {
+        self.engine.create_font_scaled(font, size)
+    }
+
+    pub fn build_text_run<S: AsRef<str>>(&mut self, font: FontScaled, text: S) -> TextRun {
+        self.engine
+            .build_text_run(font, text, &mut self.glyph_cache)
+    }
+
+    pub fn text_run(
+        &self,
+        sb: &mut SceneBuilder,
+        text_run: &TextRun,
+        affine: Affine,
+        align_x: Align,
+        brush: Brush,
+    ) {
+        let transform = affine * Affine::scale_non_uniform(1.0, -1.0);
+        let px = text_run.offset_x(align_x);
+        for cluster in &text_run.clusters {
+            for glyph in &cluster.glyphs {
+                let key = GlyphKey {
+                    id: glyph.id,
+                    offset: glyph.offset.fract(),
+                };
+                let path = self
+                    .glyph_cache
+                    .get(&(text_run.font.size, key))
+                    .expect("missing glyph entry");
+                let advance = px + glyph.offset.trunc().f64();
+
+                sb.fill(
+                    Fill::NonZero,
+                    Affine::translate((advance as _, 0.0)) * transform,
+                    &brush,
+                    None,
+                    &path,
+                );
+            }
+        }
+    }
 }
